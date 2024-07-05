67e1e95b10dda05aa374a3041ebf7febe509210a
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2024-07-05 17:56:45 -0400

    crear database mariadb

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@db/{os.getenv('MYSQL_DATABASE')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from app import routes
+db = SQLAlchemy(app)
+
+from app import routes, models
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.9-slim
-WORKDIR /Users/eithelgonzalez/Library/CloudStorage/OneDrive-Personal/Documentos/Eithel/DevProjects/lab-system
+WORKDIR /app
 COPY requirements.txt requirements.txt
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
