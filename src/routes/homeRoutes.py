e2c890e7927f8a54c8f02ac0fec6f8d4225be446
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2024-08-05 23:41:32 -0400

    add nucles & request panel home admin dashboard

--- src/routes/homeRoutes.py
+++ src/routes/homeRoutes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-from src.models import UserAccount, Project, Machine, Solvent, Sample, SamplePreparation, Request
+from src.models import UserAccount, Project, Machine, Solvent, Sample, SamplePreparation, Request, Nucleo
 from src import db
 
 home_bp = Blueprint('home', __name__)
@@ -12,6 +12,9 @@ def get_statistics():
     total_solventes = Solvent.query.count()
     total_muestras = Sample.query.count()
     total_preparaciones = SamplePreparation.query.count()
+    total_nucleos = Nucleo.query.count()
+    total_solicitudes = Request.query.count()
+
     return {
         'total_usuarios': total_usuarios,
         'total_proyectos': total_proyectos,
@@ -19,26 +22,9 @@ def get_statistics():
         'total_solventes': total_solventes,
         'total_muestras': total_muestras,
         'total_preparaciones': total_preparaciones,
+        'total_nucleos': total_nucleos,
+        'total_solicitudes': total_solicitudes
     }
-
-def get_operator_statistics():
-    total_solicitudes = Request.query.count()
-    total_proyectos = Project.query.count()
-
-    # Calcular la suma de los fondos restantes
-    suma_fondos = db.session.query(db.func.sum(Project.fondo)).scalar()
-    suma_fondos = round(float(suma_fondos or 0), 2)
-
-    # Obtener todas las solicitudes para mostrar en la tabla
-    solicitudes = Request.query.all()
-
-    return {
-        'total_solicitudes': total_solicitudes,
-        'total_proyectos': total_proyectos,
-        'suma_fondos': suma_fondos,
-        'solicitudes': solicitudes
-    }
-
 
 @home_bp.route('/home')
 @login_required
@@ -51,3 +37,17 @@ def home():
 def home_operator():
     stats = get_operator_statistics()
     return render_template('operador_dashboard.html', section='home', **stats)
+
+def get_operator_statistics():
+    total_solicitudes = Request.query.count()
+    total_proyectos = Project.query.count()
+
+    # Calcular la suma de los fondos restantes
+    suma_fondos = db.session.query(db.func.sum(Project.fondo)).scalar()
+    suma_fondos = round(float(suma_fondos or 0), 2)
+
+    return {
+        'total_solicitudes': total_solicitudes,
+        'total_proyectos': total_proyectos,
+        'suma_fondos': suma_fondos,
+    }
