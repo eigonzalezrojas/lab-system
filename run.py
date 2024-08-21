1c39e586211c3ddbc48588c81cd669063f8fee23
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2024-08-21 15:33:15 -0400

    change parameters dockerfile

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from src import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
